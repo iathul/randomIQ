d7691b3a9df1e52b2837604e9ae7b08a44690add
Athul Joy <a.athuljoy@gmail.com>
2023-09-13 11:29:17 +0530

    Add sample UI

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 // Load environment variables
 require('dotenv').config()
 
+// Import database connection
+const connectDB = require('./config/db')
+// Start db
+connectDB()
+
 const express = require('express')
 const app = express()
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-// Import database connection
-const connectDB = require('./config/db')
-// Start db
-connectDB()
+app.use(express.static('client'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/index.html')
+})
 
 // Api routes
 app.use('/api', require('./routes/index'))
