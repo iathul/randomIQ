330e12b3c2474731c2edaceba2b47801618c3e24
Athul Joy <a.athuljoy@gmail.com>
2023-09-13 22:13:39 +0530

    Update query to list rooms

--- controllers/lobby.js
+++ controllers/lobby.js
@@ -3,7 +3,7 @@ const Room = require('../models/room')
 // API to list available rooms
 exports.listRooms = async (req, res) => {
   try {
-    const rooms = await Room.find({ users: { $size: 1 } }).populate('users')
+    const rooms = await Room.find({}).populate('users')
     if (!rooms.length) {
       return res.status(404).send({
         message: 'No rooms found',
@@ -24,9 +24,8 @@ exports.listRooms = async (req, res) => {
 // Create new room and add user
 exports.createRoom = async (req, res) => {
   try {
-    const { roomName, userId } = req.body
+    const { roomName } = req.body
     const room = new Room({ roomName: roomName })
-    room.users.push(userId)
     await room.save()
     return res.status(200).send({
       message: 'Room created successfully.',
