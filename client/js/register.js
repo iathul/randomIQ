ae76c02fef3c802074774f3f34fcb6968c823790
Athul Joy <a.athuljoy@gmail.com>
2023-09-16 19:32:43 +0530

    Handle error during registration

--- client/js/register.js
+++ client/js/register.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const registrationForm = document.getElementById('registration-form')
+  const errorAlert = document.getElementById('error-message')
   registrationForm.addEventListener('submit', async e => {
     e.preventDefault()
     const userName = document.getElementById('userName').value
@@ -10,13 +11,20 @@ document.addEventListener('DOMContentLoaded', () => {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ userName, fullName })
       })
-      const data = await response.json()
-      if (data.auth_token) {
-        localStorage.setItem('user', JSON.stringify(data.user))
-        localStorage.setItem('authToken', data.auth_token)
-        window.location.href = '/lobby.html'
+
+      if (response.ok) {
+        const data = await response.json()
+        if (data.auth_token) {
+          localStorage.setItem('user', JSON.stringify(data.user))
+          localStorage.setItem('authToken', data.auth_token)
+          window.location.href = '/lobby.html'
+        } else {
+          console.error('Registration failed')
+        }
       } else {
-        console.error('Registration failed')
+        const errorMessage = await response.json()
+        errorAlert.textContent = errorMessage.error
+        errorAlert.classList.remove('d-none')
       }
     } catch (error) {
       console.error('Error:', error)
